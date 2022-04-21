97e3b66b89e63d85d43612e056bb8e4a859e4edc
HankPeeples <hgpeeples48@gmail.com>
2022-04-20 23:03:01 -0500

    Added Pair class for use in transactions ArrayList

---
+++ src/Pair.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Pair {
+    private String type;
+    private Double amount;
+
+    public Pair(String type, Double amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+}
